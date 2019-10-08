cd193eb145e2273de040baf38206bd5f8f7a13b0
Alon Ben Haim <alon.b.h.672@gmail.com>
2019-10-08 16:29:29 +0300

    feat(geolocation-support): added geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,6 +10,22 @@ describe('generateMessage', () => {
     const message = generateMessage(from, text);
 
     expect(message).toInclude({from, text});
+    expect(message.createdAt).toBeA('number'); 
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    const from = 'alon';
+    const coords ={
+      latitude: '1',
+      longitude:'1'
+    };
+    const url = `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`;
+
+    const message = generateLocationMessage(from, coords);
+
+    expect(message).toInclude({from, url});
     expect(message.createdAt).toBeA('number'); 
   });
 });
